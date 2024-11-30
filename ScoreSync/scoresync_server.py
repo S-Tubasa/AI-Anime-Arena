c91e31fd8b0e2b924c30c0553ee5941070ca927e
S-Tubasa <73173305+S-Tubasa@users.noreply.github.com>
2024-12-01 04:21:59 +0900

    Update log file path in scoresync_server.py

--- ScoreSync/scoresync_server.py
+++ ScoreSync/scoresync_server.py
@@ -19,7 +19,7 @@ def serve(port=9300, log_to_console=False):
         logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(message)s")
     else:
         logging.basicConfig(
-            filename="/log/scoresync/server.log",
+            filename="/log/scoresync.log",
             level=logging.INFO,
             format="%(asctime)s - %(message)s",
         )
